ed8a77acf00add2fdbbef7ef7b9ca001fbd4e2cd
Nicklas Vilhelmsen <nich2290@stud.kea.dk>
2019-11-20 14:30:49 +0100

    Updates

---
+++ precache-manifest.bc3105f6f4d84172b165d4764322c574.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdfb9e369ce871fffbb6af889c64e711",
+    "url": "/shoppinglist/index.html"
+  },
+  {
+    "revision": "c2fa5af11ccee4454185",
+    "url": "/shoppinglist/static/css/main.6311609d.chunk.css"
+  },
+  {
+    "revision": "bca140e28659a55ae031",
+    "url": "/shoppinglist/static/js/2.5a91f40e.chunk.js"
+  },
+  {
+    "revision": "c2fa5af11ccee4454185",
+    "url": "/shoppinglist/static/js/main.452f64c5.chunk.js"
+  },
+  {
+    "revision": "9c23bd38491d3bd9f36d",
+    "url": "/shoppinglist/static/js/runtime-main.e0b082b4.js"
+  }
+]);
